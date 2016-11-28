099328f6803100567bd9c1d3398d47755d911fad
Ben Hall <bhall@tampaprep.org>
2016-11-27 19:22:50 -0500

    derp--too much php

--- cleanup.sh
+++ cleanup.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-$WEB_ROOT="/users/bhall/public_html/"
-find $WEB_ROOT"downloads/" -type f -Bmin +15 -name "*.zip" -delete
+WEB_ROOT="/Users/bhall/public_html"
+find $WEB_ROOT"/downloads/" -type f -Bmin +15 -name "*.zip" -delete
 find $WEB_ROOT -type d -name "*tmp" -delete
