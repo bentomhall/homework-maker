830049a4dd8001c69dbc1805bc3df7b74cf1f7fa
Ben Hall <bhall@tampaprep.org>
2016-11-27 19:06:21 -0500

    modified cleanup script Now accounts for download location changes

--- cleanup.sh
+++ cleanup.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-$WEB_ROOT="/users/bhall/public_html"
-find $WEB_ROOT -type f -Bmin +15 -name "*.zip" -delete
-find $WEB_ROOT -type d -empty -delete
+$WEB_ROOT="/users/bhall/public_html/"
+find $WEB_ROOT"downloads/" -type f -Bmin +15 -name "*.zip" -delete
+find $WEB_ROOT -type d -name "*tmp" -delete
